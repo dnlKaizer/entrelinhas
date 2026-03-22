a530a1a556a8d713c26acd81a077fb38c841dd22
Leo Soares <106386261+leonardoSoares0121@users.noreply.github.com>
2026-03-22 14:23:02 -0300

    feat: estilização básica do componente header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,12 +7,25 @@ function AppHeader() {
             style={{
                 display: 'flex',
                 alignItems: 'center',
+                backgroundColor: '#ffffff',
+                borderBottom: '1px solid #e2e2e2',
+                height: '70px',
+                padding: '0 20px'
             }}
         >
-            <Title level={4} style={{ margin: 0 }}>
+            <Title
+                level={4}
+                style={{ 
+                    margin: 0,
+                    fontFamily: 'Lobster, cursive',
+                    color: '#001010',
+                    fontSize: '30px',
+                    fontWeight: 'normal',
+                }}
+                    >
                 EntreLinhas
             </Title>
-            
+
         </Header>
     )
 }
