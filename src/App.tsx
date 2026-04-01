5e8f07981c82f43d9e0e9178aaaad0ea90a4deac
dnlKaizer <danilokaizer@gmail.com>
2026-04-01 19:00:46 -0300

    feat: encapsulando os layouts em componentes de rota separados

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Layout } from 'antd'
-import AppHeader from './components/AppHeader'
-import AppFooter from './components/AppFooter'
 import ProtectedRoute from './routes/guards/ProtectedRoute'
 import PublicRoute from './routes/guards/PublicRoute'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import { useAuthSession } from './hooks/useAuthSession'
-
-const { Content } = Layout
-
-function DefaultLayout() {
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <AppHeader />
-      <Content style={{ backgroundColor: '#fafafa', padding: 25, marginBottom: '70px' }}>
-        <Outlet />
-      </Content>
-      <AppFooter />
-    </Layout>
-  )
-}
-
-function CleanLayout() {
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Content style={{ backgroundColor: '#fafafa', padding: 25 }}>
-        <Outlet />
-      </Content>
-    </Layout>
-  )
-}
+import DefaultLayout from './routes/layouts/DefaultLayout'
+import CleanLayout from './routes/layouts/CleanLayout'
 
 function App() {
   const { isAuthenticated, authLoading } = useAuthSession()
